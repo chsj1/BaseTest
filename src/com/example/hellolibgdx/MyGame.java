1d7776491c3c39a36d4173785c2c0e55dde781ab
黄俊东 <jundong.huang@dianping.com>
2015-09-13 21:26:20 +0800

    first commit all

---
+++ src/com/example/hellolibgdx/MyGame.java
@@ -0,0 +1,58 @@
+package com.example.hellolibgdx;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Touchpad;
+
+public class MyGame implements ApplicationListener {
+	@Override
+	public void create() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	
+	
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);// 设置背景为白色
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);// 清屏
+		
+		
+	}
+
+	@Override
+	public void resize(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
